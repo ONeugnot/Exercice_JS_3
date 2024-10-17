dabec36eba7737bc87eacac88e3e4e5acff97ac8
Olivier NEUGNOT <oliviergenilac@gmail.com>
2024-10-17 14:53:32 +0200

    feat(add) first commit

---
+++ Exercice1.js
@@ -0,0 +1,5 @@
+let nombre = 0;
+while (nombre <= 1000) {
+	if (nombre % 2 === 0) console.log(i);
+	i += 2;
+}
---
+++ Exercice2.js
@@ -0,0 +1,3 @@
+let phrase = "abcdefghijklmnopqrsutvwxyz";
+phrase.split("").reverse().join("");
+console.log(phrase.split("").reverse().join(""));
